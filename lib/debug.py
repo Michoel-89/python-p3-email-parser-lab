ea3124d9333dbb62f53141c164d8666239e4abaf
michoel <michoelr111@gmail.com>
2023-06-25 13:06:31 -0400

    complete

--- lib/debug.py
+++ lib/debug.py
@@ -3,4 +3,6 @@
 from email_address_parser import EmailAddressParser
 
 if __name__ == '__main__':
+    parser = EmailAddressParser("talk@talk.com john.jones@flatironschool.com alexa@amazon.com")
+    parser.parse()
     import ipdb; ipdb.set_trace()
--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, addresses):
+        self.addresses = addresses
+    def parse(self):
+        emails = re.split(r'\s|,|[\s,]+', self.addresses)
+        new_emails = []
+        for e in emails:
+            if len(e) > 0 and '@' in e:
+                new_emails.append(e)
+        new_emails.sort()
+        return new_emails
